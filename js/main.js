c0a926383468783cd465b601cdafc4af301d31bb
madkarmaa <madkarma99@gmail.com>
2024-01-12 09:38:32 +0100

    feat: insta dev guide init + various fixes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
-if (window.location.origin.endsWith('github.io'))
-    document.body.innerHTML = '<h1>This website is under construction, come back later</h1>';
+function objectToHTML(obj) {
+    let html = '';
+
+    function recurse(obj) {
+        for (let key in obj) {
+            if (typeof obj[key] === 'object') {
+                html += `<div id="${key}">`;
+                recurse(obj[key]);
+                html += `</div>`;
+            } else {
+                html += `<div id="${key}">${obj[key]}</div>`;
+            }
+        }
+    }
+
+    recurse(obj);
+
+    return html;
+}
+
+(async () => {
+    const devOptions = Array.from(await (await fetch('../dev-options.json')).json()).flat(1);
+
+    console.log(devOptions);
+    document.body.innerHTML += objectToHTML(devOptions);
+})();
